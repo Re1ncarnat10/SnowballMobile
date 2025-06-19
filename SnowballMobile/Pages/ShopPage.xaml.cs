9857b929384a0ac89cf4bda2c6a99e16fe9def2f
Re1ncarnat10 <128154276+Re1ncarnat10@users.noreply.github.com>
2025-06-20 01:49:39 +0200

    pages

--- SnowballMobile/Pages/ShopPage.xaml.cs
+++ SnowballMobile/Pages/ShopPage.xaml.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SnowballMobile.Pages;
+﻿namespace SnowballMobile.Pages;
 
 public partial class ShopPage : ContentPage
 {
-  public ShopPage()
+  public ShopPage(ShopPageModel viewModel)
   {
     InitializeComponent();
+    BindingContext = viewModel;
   }
 }
